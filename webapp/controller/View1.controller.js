602b66b1175d33be96fca75036af96f3c49d9268
Gaurav Shroff <gshroff@convergentis.com>
2018-06-11 12:40:38 +0000

    Chnages in View1

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -3,28 +3,45 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
 	"sap/ui/model/json/JSONModel",
-		"sap/m/ObjectMarker",
+	"sap/m/ObjectMarker",
 	"sap/m/UploadCollectionParameter",
 	"sap/m/library",
 	"sap/ui/core/format/FileSizeFormat",
 	"sap/ui/Device"
-], function(jquery,Controller, MessageToast, JSONModel,ObjectMarker,UploadCollectionParameter,MobileLibrary,FileSizeFormat,Device) {
+], function(jquery, Controller, MessageToast, JSONModel, ObjectMarker, UploadCollectionParameter, MobileLibrary, FileSizeFormat, Device) {
 	"use strict";
 
 	return Controller.extend("com.sapZSQRMBWA.controller.View1", {
 
 		onInit: function() {
-			
+
 			this.getView().byId("inspectionTable").getTable().setSelectionMode(sap.ui.table.SelectionMode.Single);
-		//	this.getView().byId("inspectionTable").getTable().attachRowSelectionChange(this.onSmartTableSelectionChange);
+			//	this.getView().byId("inspectionTable").getTable().attachRowSelectionChange(this.onSmartTableSelectionChange);
 			var oModel = new JSONModel();
 			oModel.setData("");
-			this.getView().setModel(oModel,"FindingModel");
+			this.getView().setModel(oModel, "FindingModel");
 			//selectionChange="onSmartTableSelectionChange"
 		},
-		onAfterRendering:function(){
-		
-			
+		onAfterRendering: function() {
+
+		},
+		onSmartTableEdit: function(oEvent) {
+			var DialogModel = new JSONModel();
+			var oData = oEvent.getSource().getBindingContext().getObject();
+			DialogModel.setData(oData);
+
+			if (!this._oDialogEdit) {
+				this._oDialogEdit = sap.ui.xmlfragment("com.sapZSQRMBWA.fragments.EditFinding", this);
+				this._oDialogEdit.setModel(this.getView().getModel("FindingModel"));
+				this._oDialogEdit.setContentHeight("60%");
+				this._oDialogEdit.setContentWidth("90%");
+			}
+
+			// toggle compact style
+
+			jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), this._oDialogEdit);
+			this._oDialogEdit.open();
+
 		},
 		onNewInspectionPress: function(oEvent) {
 			this.getOwnerComponent().getRouter().navTo("InspectionView", {
@@ -35,7 +52,7 @@ sap.ui.define([
 		onEditPress: function(oEvent) {
 			var oData = [];
 			var SelectedIndex = this.getView().byId("inspectionTable").getTable().getSelectedIndex();
-				
+
 			if (SelectedIndex !== -1) {
 				if (!this._oDialogEdit) {
 					this._oDialogEdit = sap.ui.xmlfragment("com.sapZSQRMBWA.fragments.EditFinding", this);
@@ -43,9 +60,9 @@ sap.ui.define([
 					this._oDialogEdit.setContentHeight("60%");
 					this._oDialogEdit.setContentWidth("90%");
 				}
-				
+
 				// toggle compact style
-				
+
 				jQuery.sap.syncStyleClass("sapUiSizeCompact", this.getView(), this._oDialogEdit);
 				this._oDialogEdit.open();
 
@@ -60,17 +77,16 @@ sap.ui.define([
 		onDialogSubmitButton: function(oEvent) {
 			this._oDialogEdit.close();
 		},
-		onInspectionPress:function(oEvent){
-		var InspectionId = oEvent.getSource().getText();
+		onInspectionPress: function(oEvent) {
+			var InspectionId = oEvent.getSource().getText();
 			this.getOwnerComponent().getRouter().navTo("InspectionView", {
 				NavFilters: "Link"
 			});
 		},
-		onSmartTableSelectionChange:function(oEvent){
-		var context = oEvent.getParameters().rowContext.getObject();
-		oEvent.getSource().getParent().getParent().getParent().getModel("FindingModel").setData(context);
+		onSmartTableSelectionChange: function(oEvent) {
+			var context = oEvent.getParameters().rowContext.getObject();
+			oEvent.getSource().getParent().getParent().getParent().getModel("FindingModel").setData(context);
 		},
-
 
 	});
 });
